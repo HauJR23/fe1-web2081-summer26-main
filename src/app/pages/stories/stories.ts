294385c616758626390881e6a5c4959d24279f77
HauJR23 <haumoc2304@gmail.com>
2026-07-15 19:52:06 +0700

    lab 5

--- src/app/pages/stories/stories.ts
+++ src/app/pages/stories/stories.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+interface Story {
+  id: number;
+  title: string;
+  author: string;
+  views: string;
+
+}
 
 @Component({
   selector: 'app-stories',
@@ -6,33 +15,38 @@ import { Component } from '@angular/core';
   templateUrl: './stories.html',
   styleUrl: './stories.css',
 })
-
 export class Stories {
-  stories = [
-    
-    {
-      title: "Dragon Ball",
-      author: "Toriyama",
-      year: 1984,
-      genre: "Action, Adventure, Fantasy",
-      views: 100000,
-      image: "https://www.bing.com/th/id/OIP.4CeYtVQ1oUqbnWbqbq-YjwHaEK?w=193&h=135&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=ImgAns&rm=2",
-    },
-    {
-      title: "Attack on Titan",
-      author: "Isayama",
-      year: 2009,
-      genre: "Action, Dark Fantasy, Post-Apocalyptic",
-      views: 600000,
-      image: "https://thf.bing.com/th/id/OIP.lDvs8YoY1mThIAvA-7-97gHaDt?w=346&h=180&c=7&r=0&o=7&cb=thfc1falcon4&dpr=1.3&pid=1.7&rm=3",
-    },
-    {
-      title: "Bleach",
-      author: "Kubo",
-      year: 2001,
-      genre: "Action, Adventure, Supernatural",
-      views: 500000,
-      image: "https://thf.bing.com/th/id/OIP.uoeJXNJIbKGY8pZdwFz3qwHaDt?w=332&h=180&c=7&r=0&o=7&cb=thfc1falcon4&dpr=1.3&pid=1.7&rm=3"
-    },
-  ];
+  stories: Story[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<Story[]>(`http://localhost:3000/stories`).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.stories = data;
+      },
+      error: () => {
+        console.log('error');
+      },
+    });
+  }
+  
+  deleteStory(id: number) {
+    const confirmDelete = confirm("Bạn có chắc muốn xóa không?");
+    if (!confirmDelete) return;
+
+    this.http.delete(`http://localhost:3000/stories/${id}`).subscribe({
+      next: () => {
+        this.stories = this.stories.filter((story) => story.id !== id);
+        alert("Xóa thành công");
+      },
+      error: () => {
+        alert("Xóa thất bại");
+      },
+    });
+  }
 }
+
+  
+ 
